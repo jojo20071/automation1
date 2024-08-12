3188c4382ce5e10323e139db2cd463beb5060728
Johannes Häderle <83701560+jojo20071@users.noreply.github.com>
2024-08-12 13:03:57 +0200

    ds

--- api/index.py
+++ api/index.py
@@ -2,26 +2,22 @@
 
 from flask import Flask, render_template, jsonify
 import time
+import threading
 
 app = Flask(__name__)
 
-# Global variables to manage loop state
-loop_running = False
-last_run_time = None
 
-# Function to run
-def run_task():
-    print("Function is running...")
+
+
+
+def print_with_delay(message, delay):
+    threading.Timer(delay, print, [message]).start()
 
 print("hi1")
 print("hi2")
-time.sleep(5)
-print("hi3 after 5 sec")
-time.sleep(20)
-print("hi3 after 20 sec")
-time.sleep(120)
-print("hi3 after 2 min")
-
+print_with_delay("hi3 after 5 sec", 5)
+print_with_delay("hi3 after 20 sec", 20)
+print_with_delay("hi3 after 2 min", 120)
 
 # Route to serve the HTML page
 @app.route('/')
